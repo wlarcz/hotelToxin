784f8f8e42f0987f10aac5b4ddb7c1d3250fee2f
Wladislaw <kvazid@gmail.com>
2020-06-06 20:27:49 +0700

    feat: установил cleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 
 module.exports = {
@@ -30,5 +31,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'styles.css',
         }),
+        new CleanWebpackPlugin(),
       ]
 }
